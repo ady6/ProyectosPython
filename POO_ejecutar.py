c581aaf601a0496d79061a0217831fc635cb345a
ady6 <82877241+ady6@users.noreply.github.com>
2022-01-01 20:02:28 +0100

    POO clases abstractas, polimorfismo

---
+++ POO_ejecutar.py
@@ -0,0 +1,17 @@
+from POO_Abstractas import Animal, Perro, Gato
+
+#genera errores porque es una clase abstracta
+
+perro=Animal() #instanciar una clase
+perro.saludar()
+perro.hablar("alto")
+
+perro=Perro()
+gato=Gato()
+perro.hablar("bajo")
+gato.hablar("alto")
+gato.saludar()
+
+Animal = [Perro(),Gato(),Perro(),Perro(),Gato()] #polimorfismo
+Animal[2].saludar()
+Animal[1].saludar()
