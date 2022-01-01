63737ee26e3b4bd7f0ba8d491ce0ac38b3a584fc
ady6 <82877241+ady6@users.noreply.github.com>
2022-01-01 20:05:31 +0100

    modulos a infectar con virus

---
+++ modulos_a_infectar/modulo.py
@@ -0,0 +1,5 @@
+
+"""
+programa a alterar por virus que muestra mensaje
+"""
+print('Hola, esta aplicacion funciona correctamente')
---
+++ modulos_a_infectar/modulo1.py
@@ -0,0 +1,10 @@
+
+"""
+programa a alterar por virus que suma 2+4
+"""
+
+def sumar (a,b):
+    c=a+b
+    return c
+
+print(sumar(2,4))
